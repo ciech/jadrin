b56640d8d99a32c0f53938189adab89e39a73b08
czarek <czarek.zawadka@gmail.com>
2013-06-07 01:52:59 +0200

    Fully featured prolog script. Not integrated with rest of the application

--- res/drinks.pl
+++ res/drinks.pl
@@ -1,58 +1,50 @@
-
-
 %facts
 ingredients([['Wsciekly'],['Pies']]	,[['wodka'],[['sok'], ['malinowy']],['tabasko']]).
 ingredients([['Wolski'],['Kafar']]	,[['wodka'],['pieprz']]).
 ingredients(['Mojito'],[['rum']		,[['sok'] ,['limonki']],['limonka'],['lod'],[['cukier'],['trzcinowy']]]).
 ingredients([['White'],['Malibu']]	,[['malibu'],['mleko']]).
 ingredients([['Black'],['Malibu']]	,[['malibu'],['olej'], ['samochodowy']]).
+ingredients([['Lagodny'],['Pies']]	,[['wodka'],[['sok'], ['malinowy']],['tabasko'], ['mleko']]).
 
 
 %rules
 %not_empty(L) :- E=[], L\=E.
-%takie , gdzie R
-%contains(I) :- findall(X,(ingredients(X,L),subset(I,L)),R), write(R), not_empty(R).
 
-%subset([A|X], Y) :- member(A,Y), subset(X,Y).
-%subset([], []).
-%subset([E|Tail], [E|NTail]):- subset(Tail, NTail).
-%subset([_|Tail],  NTail   ):- subset(Tail, NTail).
-  
-%zawiera_cos(I, X) :- ingredients(X,L),member(I,L).
-
-%join([ ], L2, L2).
-%join([X|L1], L2, [X|L3]):- join(L1, L2, L3). 
-%sublist(S, L):-join(L1, L2, L), join(S, L3, L2). 
-
-%zawiera_cos(I, X) :- ingredients(X,L),sublist(I,L).
-
-%zawiera_cos(I, X) :- ingredients(X,L),member(I,L).
-
-member_rec(M, [M|_]) :- write(M), write('  0: '), write(M), nl, true.
-%member_rec(M, [[H]|_]) :- write(M), write('  1 HEAD:: '), write(H), nl, member_rec(M,H).
-
-member_rec(M, [H|T]) :- write(M), write('  2 HEAD: '), write(H), write('  '), write(T), nl, ( member_rec(M,T) ; member_rec(M,H) ).
-%member_rec(['sok'],[['wodka'],[['sok'], ['malinowy']],['tabasko']]).
+member_rec(M, [M|_]) :- true.
+member_rec(M, [H|T]) :- ( member_rec(M,T) ; member_rec(M,H) ).
 
 
-%isIngredient(I) :- zawiera_cos(I, L).
+remove(X, [X|Ogon], Ogon).
+remove(X, [Y|Ogon], [Y|Ogon1]) :- remove(X, Ogon, Ogon1). 
 
-%isDrink(I) :- ingredients(I,L).
 
-%reguly potrzebne do rozwiazania:
-%pytanie od kelnera czym jest "składnik":
-%isIngredient :- 
-%isDrink :- 
+deletelist([], _, []).                  
+deletelist([X|Xs], Y, Z) :- member(X, Y), deletelist(Xs, Y, Z), !.
+deletelist([X|Xs], Y, [X|Zs]) :- deletelist(Xs, Y, Zs).
+%============================================================================
+%============================================================================
+%Ptanie zero - co to jest za typ?
+is_ingredient(I) :- ingredients(N, S), member_rec(I,S).
+is_drink(I) :- ingredients(N,S), member_rec(I,N).
 
-%Ptanie zero - co to jest
-isIngredient(I) :- ingredients(N, S), member_rec(I,S).
-isDrink(I) :- ingredients(N,S), member_rec(I,N).
-
+%===
 %A. Pytanie jak robi się rum z colą?
-%wystarczy ingredients
+%wystarczy ingredients([['Wsciekly'],['Pies']], X). + zwrócić przepis.
 
-%B. Mam składnik1 i składnik2, co mogę zrobić?.
+%===
+%B. Mam składnik1 i składnik2, co mogę zrobić? 
+
+%Zwraca tylko te drinki,ktore wykorzystuja wszystkie podane skladniki
+what_can_i_do(I, D, L) :-  ((ingredients(D,L),member(I,L)) ; length(I,0)).
+what_can_i_do([I|T],D,L) :-  (ingredients(D,L),member(I,L)) , what_can_i_do(T, D, L).
+
+%Zwraca wszystkie drinki ktore wykorzystuja ktorykolwiek skladnik
+what_can_i_do2([I|T],D,L) :-  (ingredients(D,L), (member(I,L)) ; what_can_i_do2(T, D, L)).
+
+%===
+%C. Mam cole i whisky czego mi brakuje do whisky z lodem? Zwaraca prawidlowy wynik lezeli mamy wiecej skladnikow niz jest w przepisie.
+
+what_is_missing(I,D, R) :-  ingredients(D,L), deletelist(L,I, R).
 
 
-%C. Mam cole i whisky czego mi brakuje do whisky z lodem?
 
