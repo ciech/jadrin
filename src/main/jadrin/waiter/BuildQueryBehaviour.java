49baeafc0b29df1eeee0b3335ad90f88dfefea99
ExoltenOne <krzysztof.madej.1987@gmail.com>
2013-06-07 21:26:51 +0200

    Added analize user query

--- src/main/jadrin/waiter/BuildQueryBehaviour.java
+++ src/main/jadrin/waiter/BuildQueryBehaviour.java
@@ -1,6 +1,7 @@
 	package main.jadrin.waiter;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 import main.jadrin.ontology.CheckElement;
 import main.jadrin.ontology.QueryOntology;
@@ -47,10 +48,14 @@ public class BuildQueryBehaviour extends OneShotBehaviour {
 		String[] tokens = Tokenizer.getTokens(stemmer, query, notPermitted);
 		
 		String drink = "";
+        ArrayList<String> ingredients = new ArrayList<String>();
 		for (String str: tokens)
 		{
 			ACLMessage query = new ACLMessage(ACLMessage.REQUEST);
-			query.addReceiver(bartenders[0]);
+			//query.addReceiver(bartenders[0]);
+			for(AID bartender: bartenders)
+				query.addReceiver(bartender);
+			
 			query.setOntology(QueryOntology.NAME);
 			query.setLanguage(((WaiterAgent)myAgent).getCodecName());
 			CheckElement toCheck = new CheckElement();
@@ -58,54 +63,72 @@ public class BuildQueryBehaviour extends OneShotBehaviour {
 			toCheck.setPartOf(false);
 			toCheck.setType(Type.UNKNOWN);
 			AgentAction act = toCheck;
-			Action actOperator = new Action(bartenders[0], act);
-			try {
-				myAgent.getContentManager().fillContent(query,actOperator);
-			   }
-			   catch (Exception ex) { ex.printStackTrace(); }
-			myAgent.send(query);
-			ACLMessage msg = myAgent.blockingReceive(MessageTemplate.MatchPerformative(ACLMessage.INFORM));
-		
-						try {
-					ContentElement ce =  myAgent.getContentManager().extractContent(msg);
-					if (ce instanceof Action)
-					{
-						 toCheck = (CheckElement)((Action) ce).getAction(); 
+			for(AID bartender: bartenders)
+			{
+				Action actOperator = new Action(bartender, act);
+				try {
+					myAgent.getContentManager().fillContent(query,actOperator);
+				   }
+				   catch (Exception ex) { ex.printStackTrace(); }
+				myAgent.send(query);
+				ACLMessage msg = myAgent.blockingReceive(MessageTemplate.MatchPerformative(ACLMessage.INFORM));
+			
+							try {
+						ContentElement ce =  myAgent.getContentManager().extractContent(msg);
+						if (ce instanceof Action)
+						{
+							 toCheck = (CheckElement)((Action) ce).getAction(); 
+						}
+			
+					} catch (UngroundedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (CodecException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (OntologyException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-		
-				} catch (UngroundedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (CodecException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (OntologyException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-						
-				if (toCheck.getType() == Type.DRINK)
-				{
-					drinkQuery=true;
-					drink = toCheck.getName();
-				}
-				else if (toCheck.getType() == Type.INGREDIENT)
-				{
-					ingredientQuery=true;
-					drink = toCheck.getName();
-				}
+							
+					if (toCheck != null && toCheck.getType() == Type.DRINK)
+					{
+						drinkQuery=true;
+						drink = toCheck.getName();
+					}
+					else if (toCheck != null && toCheck.getType() == Type.INGREDIENT)
+					{
+						ingredientQuery=true;
+						if(ingredients.contains(toCheck.getName()))
+							ingredients.add(toCheck.getName());
+					}
+			}
 				
 		}
 		
 		String result = "";
 		
-		if (drinkQuery)
+		if (drinkQuery && !ingredientQuery)
 		{
 			result = "Spytałeś o skład " + drink;
 		}
-		else if (ingredientQuery)
+		else if (!drinkQuery && ingredientQuery)
 		{
-			result = "Spytałeś o skład " + drink;
+			String ing = "";
+			for(String ingredient : ingredients)
+				ing += ingredient + ", ";
+			ing = ing.substring(0, ing.length() - 2);
+			
+			result = "Spytałeś co można zrobić z " + ing;
+		}
+		else if (drinkQuery && ingredientQuery)
+		{
+			String ing = "";
+			for(String ingredient : ingredients)
+				ing += ingredient + ", ";
+			ing = ing.substring(0, ing.length() - 2);
+			
+			result = "Spytałeś czego brakuje Ci do " + drink + " mając " + ing;
 		}
 		else
 		{
