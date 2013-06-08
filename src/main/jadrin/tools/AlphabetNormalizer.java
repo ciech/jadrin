366a44b0a9d3b738209f41cafede2ecdecc27591
ciech <marcin.ciechowicz@gmail.com>
2013-06-08 04:10:57 +0200

    cleaning

--- src/main/jadrin/tools/AlphabetNormalizer.java
+++ src/main/jadrin/tools/AlphabetNormalizer.java
@@ -15,8 +15,7 @@ public class AlphabetNormalizer {
   private static final Charset CHARSET = Charset.forName("UTF-8");
 		
   public static String unAccent(String s) {
-  	//forcing encoding to UTF-8, because values can be in other encoding types, so normalization can go wrong
-		CharsetDecoder decoder = CHARSET.newDecoder();
+  		CharsetDecoder decoder = CHARSET.newDecoder();
 		CharsetEncoder encoder = CHARSET.newEncoder();
 		try {
 		    ByteBuffer bbuf = encoder.encode(CharBuffer.wrap(s));		
@@ -25,9 +24,8 @@ public class AlphabetNormalizer {
 		} catch (CharacterCodingException e) {
 		}
   	  
-  	//nasty trick, for some reason Java Normalizer.Form.NFD does not normalize polish wird "l" letter
-  	s = s.replace('ł', 'l').replace('Ł', 'L');	//for changing wird "l" in UTF-8
-  	s = s.replace((char) 322, (char) 'l').replace((char) 321, (char) 'L');	//for changing wird "l" in HTML encoding
+  	s = s.replace('ł', 'l').replace('Ł', 'L');
+  	s = s.replace((char) 322, (char) 'l').replace((char) 321, (char) 'L');
   	String result =   Normalizer
 	           .normalize(s, Normalizer.Form.NFD)
 	           .replaceAll("[^\\p{ASCII}]", "");
